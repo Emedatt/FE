42a389fde2655b93292be3b5ee71e57047ca2627
Anthony <enedahanthony54@gmail.com>
2025-02-05 15:27:21 +0100

    Corrected footer logo import

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Logo from '../images/Logo.svg';
+import Logo from '../images/logo.svg';
 import facebook from '../images/facebook.svg';
 import twitter from '../images/twitter.svg';
 import instagram from '../images/instagram.svg';
